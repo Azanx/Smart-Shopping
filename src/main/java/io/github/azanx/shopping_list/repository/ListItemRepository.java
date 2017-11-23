cd389911ce95b477153ec3ff8b520ff7c556f325
Azanx <2915102+Azanx@users.noreply.github.com>
2017-11-23 20:44:39 +0100

    add findByName to repository interfaces

--- src/main/java/io/github/azanx/shopping_list/repository/ListItemRepository.java
+++ src/main/java/io/github/azanx/shopping_list/repository/ListItemRepository.java
@@ -1,9 +1,11 @@
 package io.github.azanx.shopping_list.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import io.github.azanx.shopping_list.domain.ListItem;
 
 public interface ListItemRepository extends CrudRepository<ListItem, Long> {
-
+	Collection<ListItem> findByParentListId(Long parentListId);
 }
--- src/main/java/io/github/azanx/shopping_list/repository/ShoppingListRepository.java
+++ src/main/java/io/github/azanx/shopping_list/repository/ShoppingListRepository.java
@@ -1,11 +1,12 @@
 package io.github.azanx.shopping_list.repository;
 
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.data.repository.CrudRepository;
 
 import io.github.azanx.shopping_list.domain.ShoppingList;
 
 public interface ShoppingListRepository extends CrudRepository<ShoppingList, Long> {
-	List<ShoppingList> findById(Long Id);
+	Collection<ShoppingList> findById(Long Id);
+	Collection<ShoppingList> findByOwnerUserName(String ownerUserName);
 }
