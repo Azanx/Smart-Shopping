317376df489e10e9f2f62081772379ff0a46c0f8
Azanx <2915102+Azanx@users.noreply.github.com>
2017-11-28 00:50:00 +0100

    refactor ListTooLongException and add to RestControllerAdvice

--- src/main/java/io/github/azanx/shopping_list/domain/exception/ListTooLongException.java
+++ src/main/java/io/github/azanx/shopping_list/domain/exception/ListTooLongException.java
@@ -23,28 +23,21 @@ public class ListTooLongException  extends RuntimeException implements Messageab
 		ITEM_LIST
 	}
 	
-	private final int code;
 	private final long listId; 
 	private final listType type;
 	private final HashMap<String, String> parameters;
 
 	public ListTooLongException(listType type, long listId) {
 		super("List too long! Requested list size is greater than maximum of '" + Short.MAX_VALUE +"' for '" + type + " with ID: '" + listId);
-		this.code = 404;
 		this.listId = listId;
 		this.type = type;
 		parameters = new HashMap<>();
-		parameters.put("code", Integer.toString(code));
 		parameters.put("listId", Long.toString(listId));
 		parameters.put("listType", type.toString());
 		parameters.put("message", this.getMessage());
 		
 	}
 	
-	public int getCode() {
-		return code;
-	}
-
 	public long getListId() {
 		return listId;
 	}
--- src/main/java/io/github/azanx/shopping_list/rest/RestApiControllerAdvice.java
+++ src/main/java/io/github/azanx/shopping_list/rest/RestApiControllerAdvice.java
@@ -1,11 +1,11 @@
 package io.github.azanx.shopping_list.rest;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import io.github.azanx.shopping_list.domain.exception.ListTooLongException;
 import io.github.azanx.shopping_list.exception.MessageableException;
 import io.github.azanx.shopping_list.exception.MessageableExceptionResponse;
 import io.github.azanx.shopping_list.service.exception.DuplicateUserException;
@@ -15,13 +15,11 @@ import io.github.azanx.shopping_list.service.exception.UserNotFoundException;
 @RestControllerAdvice("io.github.azanx.shopping_list.rest")
 public class RestApiControllerAdvice {
 
-	
-	
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<MessageableExceptionResponse> userNotFound(final UserNotFoundException e) {
 		return response(e, HttpStatus.NOT_FOUND);
 	}
-		
+
 	@ExceptionHandler(DuplicateUserException.class)
 	public ResponseEntity<MessageableExceptionResponse> duplicateUser(final DuplicateUserException e) {
 		return response(e, HttpStatus.CONFLICT);
@@ -31,15 +29,24 @@ public class RestApiControllerAdvice {
 	public ResponseEntity<MessageableExceptionResponse> listNotFoud(final ListNotFoundException e) {
 		return response(e, HttpStatus.NOT_FOUND);
 	}
-	
+
+	@ExceptionHandler(ListTooLongException.class)
+	public ResponseEntity<MessageableExceptionResponse> listTooLong(final ListTooLongException e) {
+		// best idea for status code I got for this one... server refuses to store new list element due to exceeding max size for the list
+		return response(e, HttpStatus.INSUFFICIENT_STORAGE);
+	}
+
 	/**
-	 * @param e - Exception from which to exctract parameters, must implement MessageableException interface
-	 * @param returnStatus - 
+	 * @param e
+	 *            - Exception from which to exctract parameters, must implement
+	 *            MessageableException interface
+	 * @param returnStatus
+	 *            -
 	 * @return ResponseEntity which can be sent back to the client
 	 */
 	private ResponseEntity<MessageableExceptionResponse> response(MessageableException e, HttpStatus returnStatus) {
-		HttpHeaders headers = new HttpHeaders();
-		MessageableExceptionResponse response = new MessageableExceptionResponse(e.getParameters(), returnStatus.value());
+		MessageableExceptionResponse response = new MessageableExceptionResponse(e.getParameters(),
+				returnStatus.value());
 		return new ResponseEntity<MessageableExceptionResponse>(response, returnStatus);
 	}
 }
