ec19dbe86ff740389ecd3213cb93834dd6abcdc0
Azanx <2915102+Azanx@users.noreply.github.com>
2017-11-28 19:13:06 +0100

    add new find methods

--- src/main/java/io/github/azanx/shopping_list/repository/ListItemRepository.java
+++ src/main/java/io/github/azanx/shopping_list/repository/ListItemRepository.java
@@ -8,4 +8,5 @@ import io.github.azanx.shopping_list.domain.ListItem;
 
 public interface ListItemRepository extends CrudRepository<ListItem, Long> {
 	Collection<ListItem> findByParentListId(Long parentListId);
+	Collection<ListItem> findByParentListIdAndParentListOwnerUserName(Long parentListId, String parentListOwnerUserName);
 }
--- src/main/java/io/github/azanx/shopping_list/repository/ShoppingListRepository.java
+++ src/main/java/io/github/azanx/shopping_list/repository/ShoppingListRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import io.github.azanx.shopping_list.domain.ShoppingList;
 
 public interface ShoppingListRepository extends CrudRepository<ShoppingList, Long> {
-	Collection<ShoppingList> findById(Long Id);
+	ShoppingList findById(Long Id);
+	ShoppingList findByIdAndOwnerUserName(Long id, String userName);
 	Collection<ShoppingList> findByOwnerUserName(String ownerUserName);
 }
