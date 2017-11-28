158012db8bc435657ec15e9d123eb2bb7f2c7396
Azanx <2915102+Azanx@users.noreply.github.com>
2017-11-28 23:22:05 +0100

    refactor + add new tests

--- src/test/java/io/github/azanx/shopping_list/service/UserServiceTest.java
+++ src/test/java/io/github/azanx/shopping_list/service/UserServiceTest.java
@@ -3,6 +3,7 @@
  */
 package io.github.azanx.shopping_list.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
@@ -19,6 +20,7 @@ import io.github.azanx.shopping_list.repository.AppUserRepository;
 import io.github.azanx.shopping_list.repository.ListItemRepository;
 import io.github.azanx.shopping_list.repository.ShoppingListRepository;
 import io.github.azanx.shopping_list.service.exception.DuplicateUserException;
+import io.github.azanx.shopping_list.service.exception.ListNotFoundException;
 import io.github.azanx.shopping_list.service.exception.UserNotFoundException;
 
 /**
@@ -38,6 +40,8 @@ public class UserServiceTest {
 	ShoppingListRepository shoppingListRepository;
 
 	UserService userService;
+	AppUser userUnderTest;
+	String userName = "User";
 
 	// clean repositories, create fresh userService instance
 	@Before
@@ -50,15 +54,29 @@ public class UserServiceTest {
 		userService = new UserService(appUserRepository, listItemRepository, shoppingListRepository);
 		userService.init(); // have to call it explicitly as I'm not autowiring
 							// the class so POST_CONSTRUCT wouldn't start
-
+		userUnderTest = new AppUser(userName, "password", "email@test.com");
+		userService.addUser(userUnderTest);
 	}
 
-	// admin should be present after initialising the database during
-	// userService construction
+	/**
+	 * admin should be present after initialising the database during
+	 * userService construction
+	 */
 	@Test
 	public void isAdminPresent() {
 		AppUser userUnderTest = userService.getUserIfExistsElseThrow("admin");
 		assertNotNull(userUnderTest);
+	}
+	
+	@Test
+	public void addNewUserSucceeds() {
+		//user already added during @Before
+		assertNotNull(userService.getUserIfExistsElseThrow(userName));
+	}
+	
+	@Test(expected = UserNotFoundException.class)
+	public void getNonExistendUserThrows() {
+		userService.getUserIfExistsElseThrow("fake");
 	}
 
 	@Test(expected = UserNotFoundException.class)
@@ -66,24 +84,49 @@ public class UserServiceTest {
 		userService.getUserIfExistsElseThrow("fake_accout");
 	}
 
-	@Test
-	public void addUserSuccessfull() {
-		AppUser userUnderTest = new AppUser("User1", "password", "email@test.com");
-		userService.addUser(userUnderTest);
-	}
-
 	@Test(expected = DuplicateUserException.class)
 	public void addDuplicateUserFails() {
-		AppUser userUnderTest = new AppUser("User1", "password", "email@test.com");
-		userService.addUser(userUnderTest);
 		userService.addUser(userUnderTest);
 	}
 	
 	@Test(expected = DuplicateUserException.class)
 	public void addDuplicateUserWithDifferentObjectsFails() {
-		AppUser userUnderTest1 = new AppUser("User1", "password", "email@test.com");
-		AppUser userUnderTest2 = new AppUser("User1", "password", "email@test.com");
-		userService.addUser(userUnderTest1);
+		AppUser userUnderTest2 = new AppUser("User", "password1", "email1@test.com");
 		userService.addUser(userUnderTest2);
 	}
+	
+	@Test(expected = ListNotFoundException.class)
+	public void getShoppingListsForNonExistentUserFails() {
+		userService.getShoppingListsForUser("fake_account");
+	}
+	
+	@Test(expected = ListNotFoundException.class)
+	public void getShoppingListsForUserWhenNoListsFails() {
+		userService.getShoppingListsForUser(userName);
+	}
+	
+	/**
+	 * Using separate AppUser instance to separate this test from methods like "update" "addList" etc
+	 */
+	@Test
+	public void getShoppingListsForUserWhithOneList() {
+		String userName = "Test2"; //hiding class field with the same name
+		AppUser userUnderTest = new AppUser(userName, "password", "email@test.com"); //hiding class field with the same name
+		userUnderTest.addShoppingList("test list");
+		userService.addUser(userUnderTest);
+		assertEquals(1, userService.getShoppingListsForUser(userName).size());
+	}
+	
+	@Test
+	public void getShoppingListsForUserWhithMultipleLists() {
+		String userName = "Test2"; //hiding class field with the same name
+		AppUser userUnderTest = new AppUser(userName, "password", "email@test.com"); //hiding class field with the same name
+		int amount = 3; //how many lists to add
+		for(int i=0; i<amount; i++)
+			userUnderTest.addShoppingList("test list"); //lists can have duplicate names
+		userService.addUser(userUnderTest);
+		assertEquals(amount, userService.getShoppingListsForUser(userName).size());
+	}
+//getShoppingListsForUser
+//getItemsForUsersListI
 }
