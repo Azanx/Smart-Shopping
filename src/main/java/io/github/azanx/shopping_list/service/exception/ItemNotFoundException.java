7c3f7fb1eac45956cd1d833527cbfe8271660f01
Azanx <2915102+Azanx@users.noreply.github.com>
2017-11-29 13:59:15 +0100

    correct field name

--- src/main/java/io/github/azanx/shopping_list/service/exception/ItemNotFoundException.java
+++ src/main/java/io/github/azanx/shopping_list/service/exception/ItemNotFoundException.java
@@ -16,27 +16,27 @@ public class ItemNotFoundException extends RuntimeException implements Messageab
 	 */
 	private static final long serialVersionUID = -4321875051376072725L;
 	private final String userName;
-	private final long itemId; //-1 if there are no items
+	private final short itemNo; //-1 if there are no items
 	private final HashMap<String, String> parameters;
 
-	public ItemNotFoundException(long itemId, String userName) {
-		super("Couldn't find list '" + itemId + "' for user: '" + userName + "'");
+	public ItemNotFoundException(short itemNo, String userName) {
+		super("Couldn't find list '" + itemNo + "' for user: '" + userName + "'");
 		this.userName = userName;
-		this.itemId = itemId;
+		this.itemNo = itemNo;
 
 		parameters = new HashMap<>();
 		parameters.put("userName", userName);
-		parameters.put("listId", Long.toString(itemId));
+		parameters.put("listId", Short.toString(itemNo));
 		parameters.put("message", this.getMessage());
 	}
 	
 	public ItemNotFoundException(String userName) {
 		super("Couldn't find any list for user: '" + userName + "'");
 		this.userName = userName;
-		this.itemId = -1;
+		this.itemNo = -1;
 		parameters = new HashMap<>();
 		parameters.put("userName", userName);
-		parameters.put("listId", Long.toString(itemId));
+		parameters.put("listId", Short.toString(itemNo));
 		parameters.put("message", this.getMessage());
 	}
 
@@ -44,8 +44,8 @@ public class ItemNotFoundException extends RuntimeException implements Messageab
 		return userName;
 	}
 
-	public long getItemId() {
-		return itemId;
+	public long getItemNo() {
+		return itemNo;
 	}
 
 	@Override
