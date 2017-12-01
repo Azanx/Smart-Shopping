43d63656797e3c809bb376b47bb021a73cd5db19
Azanx <2915102+Azanx@users.noreply.github.com>
2017-12-02 00:18:09 +0100

    temporarily hardcode user as 'admin', set home_page to redirect to list page, add support saving new lists

--- src/main/java/io/github/azanx/shopping_list/controller/MvcController.java
+++ src/main/java/io/github/azanx/shopping_list/controller/MvcController.java
@@ -9,13 +9,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import io.github.azanx.shopping_list.domain.AppUser;
 import io.github.azanx.shopping_list.domain.ShoppingList;
+import io.github.azanx.shopping_list.domain.ShoppingListDTO;
 import io.github.azanx.shopping_list.service.UserService;
 
 /**
@@ -23,75 +26,102 @@ import io.github.azanx.shopping_list.service.UserService;
  *
  */
 @Controller
-@RequestMapping("/{userName}")
+@RequestMapping("/")
 public class MvcController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MvcController.class);
 	private final UserService userService;
-	private String userName; //populated by @ModelAttribute method used for easier access inside controller methods
-	
+	private String userName; // populated by @ModelAttribute method used for
+								// easier access inside controller methods
+
 	@Autowired
 	public MvcController(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	/**
-	 * @param userName name of the user to retrieve
+	 * @param userName
+	 *            name of the user to retrieve
 	 * @return userName attribute
 	 */
 	@ModelAttribute("userName")
-	String getUserName(@PathVariable String userName){
-		LOGGER.debug("Controller for user {}",userName);
-		
+	String getUserName() {
+		String userName = "admin";
+		// TODO change this method after implementing spring security
+		LOGGER.debug("Controller for user {}", userName);
+
 		this.userName = userName;
 		return userName;
 	}
-	
+
+	/**
+	 * Send user to home page, currently just redirect to "list" page showing
+	 * all his list. May change in future
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public String home() {
+		LOGGER.debug("home() method of MvcController called for user: {}", userName);
+		return "redirect:list";
+	}
+
 	/**
 	 * Send user to a page showing all his lists
 	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView home() {
-		LOGGER.debug("home() method of MvcController called for user: {}",userName);
-		
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView showLists() {
+		LOGGER.debug("home() method of MvcController called for user: {}", userName);
+
 		ModelAndView mav = new ModelAndView("showAllLists");
 		Set<ShoppingList> shoppingLists = userService.getShoppingListsForUser(userName);
 		mav.addObject("shoppingLists", shoppingLists);
+		mav.addObject("newList", new ShoppingListDTO());
 		return mav;
 	}
-	
-	@RequestMapping(value="/list", method = RequestMethod.POST)
-	public ModelAndView addList(@ModelAttribute("newListName") String newListName) {
-		LOGGER.debug("addList() method of MvcController called for user: {}",userName);
-		
-		userService.addShoppingListToUserByName(userName, newListName);
-		//TODO implement
-		return null;
+
+	@RequestMapping(value = "/list", method = RequestMethod.POST)
+	public String addList(@ModelAttribute("newList") ShoppingListDTO newList, BindingResult result) {
+		LOGGER.debug("addList() method of MvcController called for user: {}", userName);
+
+		userService.addShoppingListToUserByName(userName, newList.getListName());
+
+		return "redirect:list";
 	}
-	
+
 	/**
 	 * Send user to a page showing all items in given list
-	 * @param listNo of the shopping list whose listItems to retrieve (listNo field, not Id used as primary key in database)
+	 * 
+	 * @param listNo
+	 *            of the shopping list whose listItems to retrieve (listNo
+	 *            field, not Id used as primary key in database)
 	 */
 	@RequestMapping(value = "/list/{listNo}", method = RequestMethod.GET)
 	public ModelAndView showLists(String userName, @PathVariable Short listNo) {
-		LOGGER.debug("showLists() method of MvcController called for user: {}",userName);
-		
+		LOGGER.debug("showLists() method of MvcController called for user: {}", userName);
+
 		ModelAndView mav = new ModelAndView("showList");
 		mav.addObject("userName", userName);
-		//TODO implement
+		// TODO implement
 		return mav;
 	}
-	
+
 	/**
 	 * Send user to a page showing his profile (information about his account)
 	 */
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
 	public ModelAndView showUserProfile() {
-		LOGGER.debug("showUserProfile() method of MvcController called for user: {}",userName);
-		
+		LOGGER.debug("showUserProfile() method of MvcController called for user: {}", userName);
+
 		ModelAndView mav = new ModelAndView("userProfile");
-		//TODO implement
+		// TODO implement
+		return mav;
+	}
+
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public ModelAndView editUserProfile(@ModelAttribute AppUser appUser) {
+		LOGGER.debug("showUserProfile() method of MvcController called for user: {}", userName);
+
+		ModelAndView mav = new ModelAndView("userProfile");
+		// TODO implement
 		return mav;
 	}
 }
