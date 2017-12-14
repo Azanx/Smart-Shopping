fdc314655c6d827da7b9668c712307fb14bc6d16
Azanx <2915102+Azanx@users.noreply.github.com>
2017-12-15 00:47:03 +0100

    add AppUserDetailsService implementing UserDetailsService for spring security usage

---
+++ src/main/java/io/github/azanx/shopping_list/service/AppUserDetailsService.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package io.github.azanx.shopping_list.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import io.github.azanx.shopping_list.domain.AppUserSecurityPrincipal;
+import io.github.azanx.shopping_list.repository.AppUserRepository;
+
+/**
+ * AppUser Service for usage by Spring Security
+ * 
+ * @author Kamil Piwowarski
+ *
+ */
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+	private AppUserRepository userRepository;
+
+	@Autowired
+	public AppUserDetailsService(AppUserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetailsService#
+	 * loadUserByUsername(java.lang.String)
+	 */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		AppUserSecurityPrincipal userPrincipal = new AppUserSecurityPrincipal(// 
+				userRepository.findByUserName(username)//
+					.orElseThrow(//
+						() -> new UsernameNotFoundException(username)));//
+		return userPrincipal;
+	}
+
+}
