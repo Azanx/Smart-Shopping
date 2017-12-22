36f402fce9ec15a87874bb3d172e5f0e6b22a658
Azanx <2915102+Azanx@users.noreply.github.com>
2017-12-22 19:50:51 +0100

    add access denied page and method security config

--- src/main/java/io/github/azanx/shopping_list/config/WebSecurityConfig.java
+++ src/main/java/io/github/azanx/shopping_list/config/WebSecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 import io.github.azanx.shopping_list.service.AppUserDetailsService;
 
@@ -22,6 +24,7 @@ import io.github.azanx.shopping_list.service.AppUserDetailsService;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -40,11 +43,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return authProvider;
 	}
 	
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler(){
+		AccessDeniedHandlerCustomImpl handler = new AccessDeniedHandlerCustomImpl();
+		handler.setErrorPage("/accessDenied");
+		return handler;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http//
 			.authorizeRequests()//
-				.antMatchers("/register", "/resources/**", "/login").permitAll()//
+				.antMatchers("/register", "/resources/**", "/login", "/accessDenied").permitAll()//
 				.anyRequest().authenticated()//
 				.and()//
 			.formLogin()//
@@ -52,6 +62,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.usernameParameter("userName")//
 				.passwordParameter("password")//
 				.permitAll()//
+				.and()//
+			.exceptionHandling()//
+				.accessDeniedHandler(accessDeniedHandler())//
 				.and()//
 			.httpBasic();//
 	}
